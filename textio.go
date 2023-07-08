22f79bb19eb8680dc54df521dc5590214f37529d
fathularif <fathulcode@gmail.com>
2023-07-08 19:16:54 +0800

    add more test

--- textio.go
+++ textio.go
@@ -4,17 +4,29 @@ import (
 	"fmt"
 )
 
+const kelantan = "Kelantenese"
+const japan = "Japanese"
 const malayHelloPrefix = "Assalam!"
+const kelantanHelloPrefix = "Guano "
+const japaneseHelloPrefix = "Konichiwa, "
 
-func Hello(name string) string {
+func Hello(name string, lang string) string {
+	if lang == kelantan {
+		return kelantanHelloPrefix + name + "? Bereh?"
+	}
+	if lang == japan {
+		return japaneseHelloPrefix + name
+	}
+
 	if name == "" {
 		name = " Apa Khabar!"
 	} else {
 		name = " Apa Khabar, " + name
 	}
+
 	return malayHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Fathul"))
+	fmt.Println(Hello("Fathul", ""))
 }
